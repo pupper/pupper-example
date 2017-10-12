bcf192a115ade4cd5e308cd0fa215da951f1e974
bouiboui <phpman1@gmail.com>
2017-10-12 14:58:35 +0200

    Added basic laravel-mix

--- app.php
+++ app.php
@@ -1,5 +1,20 @@
 <?php
 
+class HomeAction
+{
+    public function __invoke(\Aerys\Request $request, \Aerys\Response $response)
+    {
+        $response->end(
+            '<div>hello</div>' .
+            '<script src="dist/app.js"/>'
+        );
+    }
+}
+
+$router = Aerys\router()
+    ->route('GET', '/tes', new HomeAction);
+
 return (new Aerys\Host)
-    ->expose('*', 1337)
-    ->use(Aerys\root(__DIR__ . '/public'));
+    ->use($router)
+    ->use(Aerys\root(__DIR__ . '/public'))
+    ->expose('*', 1337);

d4229de62dfdcfa536c417d910b2a67caacec6e3
bouiboui <phpman1@gmail.com>
2017-10-13 14:22:41 +0200

    Created PupperComponents index

---
+++ src/components/PupperComponents.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export {default as SocketComponent} from './SocketComponent'
+export {default as SocketLogger} from './SocketLogger'
+export {default as SocketPrinter} from './SocketPrinter'
+export {default as SocketPullComponent} from './SocketPullComponent'
+export {default as SocketSubmit} from './SocketSubmit'

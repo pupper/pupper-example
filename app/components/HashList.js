b73760d7372e1817e6781f3b76a60f91338f58d8
bouiboui <phpman1@gmail.com>
2017-12-05 15:14:13 +0100

    Fixes #11 - HashList linting

--- app/components/HashList.js
+++ app/components/HashList.js
@@ -9,7 +9,12 @@ class HashList extends EventListener {
     }
 
     render () {
-        return <ul>{this.props.hashList.map((hash, key) => <li key={key}>{hash}</li>)}</ul>;
+        return <ul>
+            {this.props.hashList.map((hash, key) =>
+                <li key={key}>
+                    {hash}
+                </li>)}
+        </ul>;
     }
 }
 
